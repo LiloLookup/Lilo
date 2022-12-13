2fd6a6fb1f95126a8a11b28d84275f1e74858960
Elias <github@northernsi.de>
2022-12-13 20:24:56 +0100

    ⌨️ fix wrong url again!

--- packages/api/src/static/js/charts.js
+++ packages/api/src/static/js/charts.js
@@ -1,5 +1,5 @@
 let req = new XMLHttpRequest();
-req.open("GET", `http://localhost:3000/server/${/[^/]*$/.exec(document.location.href)[0]}/stats?size=640`, true);
+req.open("GET", `https://lilo.northernsi.de/server/${/[^/]*$/.exec(document.location.href)[0]}/stats?size=640`, true);
 
 req.onload = () => {
     const stats = JSON.parse(req.responseText);
